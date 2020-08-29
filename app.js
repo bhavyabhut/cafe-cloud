1631f62f13f3a95b26b1248f06aab7392b314ef8
Bhavya <bhavya@glabbr.com>
2020-08-29 14:07:45 +0530

    Delete Data

--- app.js
+++ app.js
@@ -1,20 +1,31 @@
 const cafeList = document.querySelector("#cafe-list");
 const cafeForm = document.querySelector("#add-cafe-form");
+
+const deleteData  = (e)=>{
+    e.stopPropagation();
+    let id = e.target.parentElement.getAttribute('data-id')
+    db.collection('cafe-cloud').doc(id).delete();
+    e.target.parentElement.remove()
+}
 const renderCafe = (doc) => {
   const data = doc.data();
   let li = document.createElement("li");
   let name = document.createElement("span");
   let city = document.createElement("span");
+  let cross = document.createElement('div');
 
   li.setAttribute("data-id", doc.id);
   name.innerHTML = data.name;
   city.innerHTML = data.city;
+  cross.textContent = 'x';
+  cross.addEventListener('click',deleteData)
 
-  li.append(name, city);
+  li.append(name, city,cross);
   cafeList.append(li);
 };
 
 db.collection("cafe-cloud")
+    .where('city','==','Rajkot')
   .get()
   .then((res) => {
     res.docs.forEach((doc) => {
